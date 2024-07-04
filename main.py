5e139a356322e98ab5f78a2915244f3ebbfc57cd
Jay2291 <jayjagtap619@gmail.com>
2024-07-04 15:38:29 +0530

    multiple office commit

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import datetime
 import random
-from flask import Flask, redirect, url_for,render_template, request, session, flash, jsonify
+from flask import Flask, request, session, jsonify
 from datetime import timedelta, datetime
 from flask_mysqldb import MySQL
 import MySQLdb.cursors 
-import time
 
 app = Flask(__name__)
 app.secret_key = "key"
@@ -31,10 +30,11 @@ def update_status(parid):
 @app.route("/home")
 def home():
     if "loggedin" in session:
-        ui = session['userid'],
-        un = session["username"],
-        add = session["address"],
+        ui = session['userid']
+        un = session["username"]
+        add = session["address"]
         phone = session["phone"]
+        postid = session["postal"]
         return jsonify({
                     "user":
                     {
@@ -42,6 +42,7 @@ def home():
                         'Username': un,
                         'Address': add,
                         'Phone no.': phone,
+                        'Postal ID': postid
                     }
                         }
                     )
@@ -62,6 +63,7 @@ def login():
             session["username"] = account["username"]
             session["address"] = account["address"]
             session["phone"] = account["phoneno"]
+            session["postal"] = account["postid"]
             return jsonify({
                 'message': 'Login Successfull',
                 
@@ -71,15 +73,16 @@ def login():
 
 @app.route("/signup", methods = ["POST", "GET"])
 def signup():
-    if request.method == "POST" and 'username' in request.json and 'address' in request.json and 'phoneno' in request.json:
+    if request.method == "POST" and 'username' in request.json and 'address' in request.json and 'phoneno' in request.json and 'postid' in request.json:
         username = request.json['username']
         address = request.json['address']
         phoneno = request.json['phoneno']
+        postal = request.json['postid']
         
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute(f"INSERT INTO user (username, address, phoneno) VALUES {username, address, phoneno}")
+        cursor.execute(f"INSERT INTO user (username, address, phoneno, postid) VALUES {username, address, phoneno, postal}")
         mysql.connection.commit()
-        cursor.execute(f"SELECT userid FROM user WHERE username = {username}")
+        cursor.execute(f"SELECT userid FROM user WHERE username = '{username}'")
         userid = cursor.fetchone()
         return jsonify({
             "message": "Account created Successfully.",
@@ -90,17 +93,18 @@ def signup():
 
 @app.route("/update", methods = ["POST", "GET"])
 def update():
-    if request.method == "POST" and 'username' in request.json and 'address' in request.json and 'phoneno' in request.json:
+    if request.method == "POST" and 'username' in request.json and 'address' in request.json and 'phoneno' in request.json and 'postid' in request.json:
         ui = session['userid']
         username = request.json['username']
         address = request.json['address']
         phoneno = request.json['phoneno']
+        postal = request.json['postid']
         
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute(f"SELECT * FROM user WHERE userid = {ui}")
         account = cursor.fetchone()
         if account:
-            cursor.execute(f"UPDATE user SET username='{username}', address='{address}', phoneno='{phoneno}' WHERE userid={ui}")
+            cursor.execute(f"UPDATE user SET username='{username}', address='{address}', phoneno='{phoneno}', postid = '{postal}' WHERE userid={ui}")
             mysql.connection.commit()
             return jsonify({"message":"Account updated Successfully"})
         else:
@@ -110,11 +114,12 @@ def update():
 
 @app.route("/send", methods = ["POST", "GET"])
 def send():
-    if request.method == "POST" and 'recname' in request.json and 'address' in request.json and 'content' in request.json and 'phoneno' in request.json:
+    if request.method == "POST" and 'recname' in request.json and 'address' in request.json and 'content' in request.json and 'phoneno' in request.json and 'postid' in request.json:
         receiver_username = request.json['recname']
         receiver_address = request.json['address']
         content = request.json['content']
         phoneno = request.json['phoneno']
+        postal = request.json['postid']
 
         sender_username = session.get("username", "")
         
@@ -125,17 +130,20 @@ def send():
         cursor.execute(f"SELECT userid FROM user WHERE username = '{receiver_username}'")
         receiver_info = cursor.fetchone()
         
-        if not receiver_info:
+        if not receiver_info: #if receiverid not found in db
             cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-            cursor.execute(f"INSERT INTO user (username, address, phoneno) VALUES {receiver_username, receiver_address, phoneno}")
+            cursor.execute(f"INSERT INTO user (username, address, phoneno, postid) VALUES {receiver_username, receiver_address, phoneno, postal}")
             mysql.connection.commit()
         cursor.execute(f"SELECT userid FROM user WHERE username = '{receiver_username}'")
         receiver_info = cursor.fetchone()
         sender_id = sender_info['userid']
         receiver_id = receiver_info['userid']
-        cursor.execute(f"INSERT INTO parcel (senderid, receiverid, contenttype, status, postid) VALUES{sender_id, receiver_id, content, 'in transit', 1}")
+        cursor.execute(f"SELECT postid FROM user WHERE username = '{sender_username}'")
+        senderpost = cursor.fetchone()
+        senderpostid = senderpost['postid']
+        cursor.execute(f"INSERT INTO parcel (senderid, receiverid, contenttype, status, senderpostid, recieverpostid) VALUES{sender_id, receiver_id, content, 'in transit', senderpostid, postal}")
         mysql.connection.commit()
-        cursor.execute(f"SELECT parcelid FROM parcel ORDER BY parcelid DESC LIMIT 1;") 
+        cursor.execute(f"SELECT parcelid FROM parcel ORDER BY parcelid DESC LIMIT 1;") #last row of the parcel table
         parid = cursor.fetchone()
         return jsonify({"message": "Parcel sent successfully",
                         "Parcel ID": parid
@@ -166,25 +174,28 @@ def recievedpost():
     if request.method == "POST" and 'postid' in request.json:
         postid = request.json["postid"]
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute(f"SELECT * FROM parcel WHERE postid = {postid}")
+        if postid == 1:
+            cursor.execute(f"SELECT * FROM parcel WHERE status = 'Left for post:1' or recieverpostid={postid}")
+        else:
+            cursor.execute(f"SELECT * FROM parcel WHERE senderpostid = {postid} or recieverpostid={postid}")
+        
         parall = cursor.fetchall()
         if parall:
             for i in parall:
-                parid = i['parcelid']  
+                parid = i['parcelid']
+                stat  = i['status']
                 res = update_status(parid)
                 if res:
-                    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-                    cursor.execute(f"SELECT status FROM parcel WHERE parcelid = {parid}")
-                    stat = cursor.fetchall()
-                    for i in stat:
-                        tus = i['status']
-                        if tus == 'in transit':
-                            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-                            cursor.execute(f"UPDATE parcel SET status = 'Acquired' WHERE parcelid ={parid}")
-                            mysql.connection.commit()
-            cursor.execute(f"SELECT * FROM parcel WHERE status='Acquired' and parcelid={parid}")
-            parcels = cursor.fetchall()
-            return jsonify({"Parcel": parcels})
+                    if stat == 'Left for post:1' and postid == 1:
+                        cursor.execute(f"UPDATE parcel SET status = 'Acquired at post:{postid}' WHERE parcelid ={parid}")
+                    elif stat == 'in transit':
+                        cursor.execute(f"UPDATE parcel SET status = 'Acquired at post:{postid}' WHERE parcelid ={parid}")
+                    else:
+                        cursor.execute(f"UPDATE parcel SET status = 'Acquired at post:{postid}' WHERE parcelid ={parid} and status LIKE 'Left for post:{postid}'")
+                    mysql.connection.commit()
+                cursor.execute(f"SELECT * FROM parcel WHERE status='Acquired at post:{postid}'")
+                parcels = cursor.fetchall()
+            return jsonify({"Parcel": parcels}) if parcels else jsonify({"message": "No parcels acquired"}), 404
         else:
             return jsonify({"message":"NO parcels at the moment"})
     else:
@@ -195,28 +206,27 @@ def sendpost():
     if request.method == "POST" and 'postid' in request.json:
         postid = request.json["postid"]
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute(f"SELECT parcelid FROM parcel WHERE postid = {postid}")
+        if postid == 1:
+            cursor.execute(f"SELECT * FROM parcel WHERE status = 'Acquired at post:1' or senderpostid = {postid} or recieverpostid={postid}")
+        else:
+            cursor.execute(f"SELECT * FROM parcel WHERE senderpostid = {postid} or recieverpostid={postid}")          
         parall = cursor.fetchall()
         if parall:
             for i in parall:
-                parid = i['parcelid']  
+                parid = i['parcelid']
+                recpostid = i['recieverpostid']  
                 res = update_status(parid)
                 if res:
-                    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-                    cursor.execute(f"SELECT status FROM parcel WHERE parcelid = {parid}")
-                    stat = cursor.fetchall()
-                    for i in stat:
-                        tus = i['status'] 
-                        if tus.lower() == 'acquired':
-                            cursor.execute(f"UPDATE parcel SET status = 'On the way' WHERE parcelid ={parid}")
-                            mysql.connection.commit()
-            cursor.execute(f"SELECT * FROM parcel WHERE status='On the way' and parcelid={parid}")
-            parcels = cursor.fetchall()
-            
-            return jsonify({"Parcel": parcels})
-           
-        else:
-            return jsonify({"message":"NO parcels at the moment"})
+                    if postid == recpostid:
+                        cursor.execute(f"UPDATE parcel SET status = 'Out for delivery' WHERE parcelid ={parid} and status ='Acquired at post:{postid}'")
+                    elif postid == 1:
+                        cursor.execute(f"UPDATE parcel SET status = 'Left for post:{recpostid}' WHERE parcelid ={parid} and status ='Acquired at post:1'")
+                    else:
+                        cursor.execute(f"UPDATE parcel SET status = 'Left for post:1' WHERE parcelid ={parid} and status ='Acquired at post:{postid}'")
+                    mysql.connection.commit()  
+                cursor.execute(f"SELECT * FROM parcel WHERE parcelid = {parid} and status = 'Out for delivery' or status LIKE 'Left for post%'")
+            parcels = cursor.fetchall()     
+            return jsonify({"Parcel": parcels}) if parcels else jsonify({"message": "No parcels at moment"}), 404
     else:
         return jsonify({"message": "Invalid request method"}), 405
 
@@ -228,24 +238,18 @@ def recieve():
         cursor.execute(f"SELECT parcelid FROM parcel WHERE receiverid = {ui}")
         receiver_info = cursor.fetchall()
         if receiver_info:
-            for i in receiver_info:   
-                parid = i['parcelid']
-                cursor.execute(f"SELECT status FROM parcel WHERE parcelid = {parid}")
-                stat = cursor.fetchall()
-                
-                for i in stat:
-                    tus = i['status']  
-                    if tus.lower() == "on the way":
-                        cursor.execute(f"UPDATE parcel SET status = 'Delivered' WHERE receiverid ={ui}")
-                        mysql.connection.commit()
+            for i in receiver_info:
+                recid = i['parcelid']
+                cursor.execute(f"UPDATE parcel SET status = 'Delivered' WHERE parcelid ={recid} and status = 'Out for delivery'")
+                mysql.connection.commit()
             cursor.execute(f"SELECT * FROM parcel WHERE status='Delivered' and receiverid={ui}")
             parcels = cursor.fetchall()
-            return jsonify({"Parcel": parcels})
-            
+            return jsonify({"Parcel": parcels}) if parcels else jsonify({"message": "No parcels delivered"}), 404
+                
         else:
-            return jsonify({"message": "No parcels found for this receiver"})
+            return jsonify({"message": "No parcels found for this user"})
     else:
-        return jsonify({"message": "Receiver ID not found"})
+        return jsonify({"message": "User ID not found"})
 
 @app.route("/logout")
 def logout():
